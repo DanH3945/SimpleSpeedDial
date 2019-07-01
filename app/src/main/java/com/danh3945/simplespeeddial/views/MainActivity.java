227db782b817c4c33ca7b0e4a517718e8ccad032
Heretic <hereticpurge@gmail.com>
2019-07-01 00:32:10 -0600

    Moved elements around the primary display layout. Added checking the orientation before popping the backstack so we don't remove the wrong fragments.

--- app/src/main/java/com/danh3945/simplespeeddial/views/MainActivity.java
+++ app/src/main/java/com/danh3945/simplespeeddial/views/MainActivity.java
@@ -1,5 +1,6 @@
 package com.danh3945.simplespeeddial.views;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -68,7 +69,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0 &&
+                !(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)) {
             getSupportFragmentManager().popBackStack();
         } else {
             super.onBackPressed();
